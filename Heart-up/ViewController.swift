242eb0c4af2f0d7570982e1df3e4d63bfa427b3b
ikedaema2018 <ikedaema0817@gmail.com>
2018-08-22 19:10:11 +0900

    ピンを５個立てる

--- Heart-up/ViewController.swift
+++ Heart-up/ViewController.swift
@@ -11,31 +11,67 @@ import MapKit
 import CoreLocation
 
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, MKMapViewDelegate  {
     @IBOutlet weak var testMapView: MKMapView!
-    //CLLocationManagerの入れ物を用意
-    var myLocationManager:CLLocationManager!
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-        //CLLocationManagerをインスタンス化
-        myLocationManager = CLLocationManager()
+        let x = 140.000000 //経度
+        let y = 35.000000  //緯度
         
+        //中心座標
+        let center = CLLocationCoordinate2DMake(y, x)
         
-        //位置情報使用許可のリクエストを表示するメソッドの呼び出し
-        myLocationManager.requestWhenInUseAuthorization()
-
+        //表示範囲
+        let span = MKCoordinateSpanMake(1.0, 1.0)
+        
+        //中心座標と表示範囲をマップに登録する。
+        let region = MKCoordinateRegionMake(center, span)
+        testMapView.setRegion(region, animated:true)
+        
+        //中心にピンを立てる。
+        let annotation = MKPointAnnotation()
+        annotation.coordinate = CLLocationCoordinate2DMake(y, x)
+        annotation.title = "中心"
+        annotation.subtitle = "\(annotation.coordinate.latitude), \(annotation.coordinate.longitude)"
+        testMapView.addAnnotation(annotation)
+        
+        //左下のピン
+        let annotation1 = MKPointAnnotation()
+        annotation1.coordinate = CLLocationCoordinate2DMake(y-1.0, x-1.0)
+        annotation1.title = "左下"
+        annotation1.subtitle = "\(annotation1.coordinate.latitude), \(annotation1.coordinate.longitude)"
+        testMapView.addAnnotation(annotation1)
+        
+        //右下のピン
+        let annotation2 = MKPointAnnotation()
+        annotation2.coordinate = CLLocationCoordinate2DMake(y-1.0, x+1.0)
+        annotation2.title = "右下"
+        annotation2.subtitle = "\(annotation2.coordinate.latitude), \(annotation2.coordinate.longitude)"
+        testMapView.addAnnotation(annotation2)
+        
+        //左上のピン
+        let annotation3 = MKPointAnnotation()
+        annotation3.coordinate = CLLocationCoordinate2DMake(y+1.0, x-1.0)
+        annotation3.title = "左上"
+        annotation3.subtitle = "\(annotation3.coordinate.latitude), \(annotation3.coordinate.longitude)"
+        testMapView.addAnnotation(annotation3)
+        
+        //右上のピン
+        let annotation4 = MKPointAnnotation()
+        annotation4.coordinate = CLLocationCoordinate2DMake(y+1.0, x+1.0)
+        annotation4.title = "右上"
+        annotation4.subtitle = "\(annotation4.coordinate.latitude), \(annotation4.coordinate.longitude)"
+        testMapView.addAnnotation(annotation4)
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    //位置情報取得に失敗したときに呼び出されるメソッド
-    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
-        print("error")
-    }
+    
+    
 
 }
 
