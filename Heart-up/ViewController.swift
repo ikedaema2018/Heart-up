f5a4d5630e9ba148235e61d30859dc75e26de6c3
ikedaema2018 <ikedaema0817@gmail.com>
2018-08-22 13:23:54 +0900

    現在地

--- Heart-up/ViewController.swift
+++ Heart-up/ViewController.swift
@@ -7,19 +7,35 @@
 //
 
 import UIKit
+import MapKit
+import CoreLocation
+
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var testMapView: MKMapView!
+    //CLLocationManagerの入れ物を用意
+    var myLocationManager:CLLocationManager!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        //CLLocationManagerをインスタンス化
+        myLocationManager = CLLocationManager()
+        
+        
+        //位置情報使用許可のリクエストを表示するメソッドの呼び出し
+        myLocationManager.requestWhenInUseAuthorization()
+
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    //位置情報取得に失敗したときに呼び出されるメソッド
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        print("error")
+    }
 
 }
 
