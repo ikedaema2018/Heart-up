ae27b7d3df81bb3c68bf3b767c56cb27749718ea
ikedaema2018 <ikedaema0817@gmail.com>
2018-10-04 13:55:45 +0900

    myPageのselfIntroductionを編集したら現れるテキストフィールドに合わせてtextViewをあげる

--- Heart-up/MyPageViewController.swift
+++ Heart-up/MyPageViewController.swift
@@ -18,7 +18,6 @@ class MyPageViewController: UIViewController {
     @IBOutlet weak var profileImage: UIImageView!
     
     @IBAction func saveButton(_ sender: Any) {
-        print(selfIntroduceView.text)
         UserRegister.updateIntroduce(intro: selfIntroduceView.text, callback: { error in
             if let error = error {
                 if let message = error["message"] {
@@ -79,6 +78,9 @@ class MyPageViewController: UIViewController {
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
+        let notificationCenter = NotificationCenter.default
+        notificationCenter.addObserver(self, selector: #selector(self.handleKeyboardWillShowNotification(_:)), name: .UIKeyboardWillShow, object: nil)
+        notificationCenter.addObserver(self, selector: #selector(self.handleKeyboardWillHideNotification(_:)), name: .UIKeyboardWillHide, object: nil)
         fetchData()
     }
 
@@ -123,7 +125,6 @@ extension MyPageViewController {
 }
 
 extension MyPageViewController: UITextViewDelegate {
-    // hides text views
     func textView(_ textView: UITextView, shouldChangeTextIn range: NSRange, replacementText text: String) -> Bool {
         
         if (text == "\n") {
@@ -136,6 +137,7 @@ extension MyPageViewController: UITextViewDelegate {
     
     // キーボードが現れた時に、画面全体をずらす。
     @objc private func handleKeyboardWillShowNotification(_ notification: Notification) {
+        print("-----dadw--------------------------")
         let userInfo = notification.userInfo //この中にキーボードの情報がある
         let keyboardSize = (userInfo![UIKeyboardFrameEndUserInfoKey] as! NSValue).cgRectValue
         let keyboardY = self.view.frame.size.height - keyboardSize.height //画面全体の高さ - キーボードの高さ = キーボードが被らない高さ
@@ -144,7 +146,6 @@ extension MyPageViewController: UITextViewDelegate {
             UIView.animate(withDuration: 0.25, delay: 0.0, options: .curveEaseIn, animations: {
                 self.view.frame = CGRect(x: 0, y: self.view.frame.origin.y - (editingTextFieldY - (keyboardY - 60)), width: self.view.bounds.width, height: self.view.bounds.height)
             }, completion: nil)
-            
         }
     }
     
