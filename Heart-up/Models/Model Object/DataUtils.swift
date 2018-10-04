dac2c5cfb472ac2db4b6a68c44ea626f63e81be6
ikedaema2018 <ikedaema0817@gmail.com>
2018-10-04 12:22:42 +0900

    データベースから文字列で拾ってきた情報を一旦日付に変換し、また、文字列に変換

---
+++ Heart-up/Models/Model Object/DataUtils.swift
@@ -0,0 +1,15 @@
+//
+//  DataUtils.swift
+//  Heart-up
+//
+//  Created by 前田啓 on 2018/10/04.
+//  Copyright © 2018年 kei maeda. All rights reserved.
+//
+
+import UIKit
+
+class DataUtils: NSObject {
+    struct MyDate: Codable {
+        let dt: Date
+    }
+}
