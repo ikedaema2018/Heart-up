7bf9e58eb87780597079344ce749c4b85a857449
ikedaema2018 <ikedaema0817@gmail.com>
2018-10-06 23:38:29 +0900

    起動時にムービー

--- Heart-up/FirstMovieViewController.swift
+++ Heart-up/FirstMovieViewController.swift
@@ -8,13 +8,15 @@
 
 import UIKit
 import AVFoundation
+import AVKit
 
 class FirstMovieViewController: UIViewController {
+    var playerView: AVPlayerView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Bundle Resourcesからsample.mp4を読み込んで再生
-        let path = Bundle.main.path(forResource: "shabonView", ofType: "mp4")!
+        let path = Bundle.main.path(forResource: "shabonView3", ofType: "mp4")!
         let player = AVPlayer(url: URL(fileURLWithPath: path))
         player.play()
         
@@ -27,6 +29,10 @@ class FirstMovieViewController: UIViewController {
         view.layer.insertSublayer(playerLayer, at: 0) // 動画をレイヤーとして追加
 
         // Do any additional setup after loading the view.
+        // 動画が再生し終わったことを監視する設定
+        NotificationCenter.default.addObserver(
+            self, selector: #selector(self.endOfMovie),
+            name: .AVPlayerItemDidPlayToEndTime, object: nil)
     }
 
     override func didReceiveMemoryWarning() {
@@ -34,6 +40,16 @@ class FirstMovieViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    @objc func endOfMovie() {
+        //遷移
+        performSegue(withIdentifier: "showLocateSegue", sender: nil)
+    }
+    
+//    override var representedObject: Any? {
+//        didSet {
+//            // Update the view, if already loaded.
+//        }
+//    }
 
     /*
     // MARK: - Navigation
---
+++ Heart-up/Models/AVPlayerView.swift
@@ -0,0 +1,6 @@
+import UIKit
+import AVFoundation
+
+class AVPlayerView : UIView {
+    
+}
