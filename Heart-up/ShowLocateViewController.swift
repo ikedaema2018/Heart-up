710736777e38cefbffd9afb12ffdf95e6e6142f4
ikedaema2018 <ikedaema0817@gmail.com>
2018-10-24 14:45:57 +0900

    再読み込み画面完成！

--- Heart-up/ShowLocateViewController.swift
+++ Heart-up/ShowLocateViewController.swift
@@ -407,13 +407,10 @@ extension ShowLocateViewController {
     
     //電波が悪い時に避難用のエラービューを表示
     func errorViewDisplay(_ message: String){
-        print("nazue")
         var errorView = UIView()
         errorView = UIView.init(frame: CGRect.init(x: 0, y: 0, width: self.view.frame.width, height: self.view.frame.height))
         errorView.backgroundColor = UIColor.white
-        self.view.addSubview(errorView)
         
-        print("nazue2")
         let errorLabel = UILabel()
         let grayColor = #colorLiteral(red: 0.2549019754, green: 0.2745098174, blue: 0.3019607961, alpha: 1)
         errorLabel.text = message
@@ -421,9 +418,7 @@ extension ShowLocateViewController {
         errorLabel.font = UIFont(name: "Arial", size: 20)
         errorLabel.frame = CGRect(x: 20, y: 100, width: self.view.frame.width - 40, height: 60)
         errorLabel.numberOfLines = 3
-        self.view.addSubview(errorLabel)
         
-        print("nazue3")
         let reload = UIButton()
         reload.setTitle("再読み込みする", for: .normal)
         let thinBlue = #colorLiteral(red: 0.2392156869, green: 0.6745098233, blue: 0.9686274529, alpha: 1)
@@ -431,7 +426,11 @@ extension ShowLocateViewController {
         reload.layer.cornerRadius = 10
         
         reload.frame = CGRect(x: 0, y: 0, width: self.view.frame.width / 3, height: 50)
+        reload.center = self.view.center
+        
+        errorView.addSubview(errorLabel)
         errorView.addSubview(reload)
+        self.view.addSubview(errorView)
     }
     
 }
