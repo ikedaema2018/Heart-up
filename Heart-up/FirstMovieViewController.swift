4691fc67d6026523890f0355eea975bad6fdc223
ikedaema2018 <ikedaema0817@gmail.com>
2018-10-25 11:25:03 +0900

    動画の再生復活

--- Heart-up/FirstMovieViewController.swift
+++ Heart-up/FirstMovieViewController.swift
@@ -16,23 +16,22 @@ class FirstMovieViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Bundle Resourcesからsample.mp4を読み込んで再生
-//        let path = Bundle.main.path(forResource: "shabonView3", ofType: "mp4")!
-//        let player = AVPlayer(url: URL(fileURLWithPath: path))
-//        player.play()
-        
+        let path = Bundle.main.path(forResource: "shabonView3", ofType: "mp4")!
+        let player = AVPlayer(url: URL(fileURLWithPath: path))
+        player.play()
         
         // AVPlayer用のLayerを生成
-//        let playerLayer = AVPlayerLayer(player: player)
-//        playerLayer.frame = view.bounds
-//        playerLayer.videoGravity = .resizeAspectFill
-//        playerLayer.zPosition = -1 // ボタン等よりも後ろに表示
-//        view.layer.insertSublayer(playerLayer, at: 0) // 動画をレイヤーとして追加
+        let playerLayer = AVPlayerLayer(player: player)
+        playerLayer.frame = view.bounds
+        playerLayer.videoGravity = .resizeAspectFill
+        playerLayer.zPosition = -1 // ボタン等よりも後ろに表示
+        view.layer.insertSublayer(playerLayer, at: 0) // 動画をレイヤーとして追加
 
         // Do any additional setup after loading the view.
         // 動画が再生し終わったことを監視する設定
-//        NotificationCenter.default.addObserver(
-//            self, selector: #selector(self.endOfMovie),
-//            name: .AVPlayerItemDidPlayToEndTime, object: nil)
+        NotificationCenter.default.addObserver(
+            self, selector: #selector(self.endOfMovie),
+            name: .AVPlayerItemDidPlayToEndTime, object: nil)
     }
 
     override func didReceiveMemoryWarning() {
@@ -47,18 +46,18 @@ class FirstMovieViewController: UIViewController {
     
 //    override var representedObject: Any? {
 //        didSet {
-//            // Update the view, if already loaded.
+            // Update the view, if already loaded.
 //        }
 //    }
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    
+//     MARK: - Navigation
+//
+//     In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+//         Get the new view controller using segue.destinationViewController.
+//         Pass the selected object to the new view controller.
     }
-    */
+    
 
 }
